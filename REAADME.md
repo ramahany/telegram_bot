7bef4eeb0d370d3c30a082b3a8eaed9d543fdea0
r4m4 <ramahany317@gmail.com>
2026-02-28 18:41:20 +0200

    sesion

--- REAADME.md
+++ REAADME.md
@@ -2,9 +2,23 @@
     => use BotFather on telegram to create you bot and get your token 
     => save this token in your .env file 
 
+
+
 ## build the docker container 
     docker build -t my_telegram_bot .
-# run the docker container 
+### run the docker container 
     docker run --rm --env-file .env my_telegram_bot
     Note: when using the .env file make sure you remove the quotes around the values
+
+## For div with docker 
+    because i use COPY it copies all the files to the image once its builtk, so to avoid that i will be using the following instruction while div.
+    CMD:
+        docker run --rm --env-file .env -v .:/app my_telegram_bot
+
+### using docker composer 
+    Yes, build: . tells docker-compose to look for a Dockerfile in the current directory and build it automatically when you run docker-compose up.
+    If you want to force a rebuild after changes to your Dockerfile or requirements:
+    docker-compose up --build
+    Otherwise docker-compose up reuses the previously built image.
+    
 ## Hosting docker using railway
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from telebot.types import Message
 from helpers.config import get_settings
 from llm import GroqClient
 from collections import defaultdict
+from datetime import datetime
 import logging
 
 # logging.basicConfig(
@@ -10,29 +11,46 @@ import logging
 #     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 # )
 logger = logging.getLogger("telegram_bot_log")
+# hist functions
+def create_inner_dict():
+    return {"hist":[], "last_active": datetime.today()}
+
+def is_session_expired(last_sesion, curr,threshold): # threshold in hours
+
+    return (curr-last_sesion).total_seconds() // 60 >= threshold 
+    # return (last_sesion-curr).total_seconds() // 3600 >= threshold
+
 
 bot_settings = get_settings()
 client = GroqClient(api_key=bot_settings.GROQ_API_KEY)
 bot = telebot.TeleBot(token=bot_settings.BOT_TOKEN)
-all_hist = defaultdict(list)
+all_hist = defaultdict(create_inner_dict)
 
 @bot.message_handler(commands=['hello']) # / + command
 def send_welcome(message:Message): 
     bot.reply_to(
         message=message,
-        text=f"hello, I am a BOT created by the most spactcaular, amazing woman on planet earth.\n her name is r4m4 you would love to know her, but she doesn't like talking to human beings so NO."
+        text=f"hello, I am a CHAT BOT created by the most spactacular, amazing woman on planet earth.\nher name is r4m4 you would love to know her, but she doesn't like talking to human beings."
     )
 
 @bot.message_handler(func= lambda message: True)
 def groq_response(message:Message): 
-    all_hist[message.chat.id].append({
+
+    curr_time = datetime.today()
+
+    if  is_session_expired(all_hist[message.chat.id]["last_active"], curr=curr_time, threshold=6): 
+        all_hist[message.chat.id]["hist"] = []
+
+    all_hist[message.chat.id]["last_active"] = curr_time    
+
+    all_hist[message.chat.id]["hist"].append({
         "role": "user",
         "content":message.text
     })
 
-    groq_response = client.response(hist=all_hist[message.chat.id])
+    groq_response = client.response(hist=all_hist[message.chat.id]["hist"])
 
-    all_hist[message.chat.id].append({
+    all_hist[message.chat.id]["hist"].append({
         "role": "assistant",
         "content":groq_response
     })
@@ -41,6 +59,7 @@ def groq_response(message:Message):
         message=message, 
         text=groq_response[:4000]
     )
+    
     # bot.send_message(
     #     chat_id=message.chat.id,
     #     text="done"
@@ -48,5 +67,6 @@ def groq_response(message:Message):
 
 if __name__ == '__main__':
     logger.info("bot is running ...")
+    print("bot is running ...")
     bot.infinity_polling()
     
